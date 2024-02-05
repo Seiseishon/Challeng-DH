33bc2ee9fe5bbcded2fdc476ad622a3dcf1161e5
Naim Apaza <naimlautaroapaza@gmail.com>
2024-02-05 01:33:07 -0300

    cambios

--- back-end/app.js
+++ back-end/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const app = express()
+
 let PORT = 3000;
-app.listen(PORT,()=> console.log(`corriendo en el puerto ${PORT}`));
+
+app.listen(PORT, () => console.log(`corriendo en el puerto ${PORT}`));
