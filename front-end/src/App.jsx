33edb05ada636bd7373594a681daa434dd73d334
HiTzieowo <tomasvac02@gmail.com>
2024-03-03 02:00:25 -0300

    importacion react

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,6 +4,8 @@ import ContentWrap from "./components/contentWrap";
 import Logo from "./components/Logo";
 import Formulario from "./components/Formulario";
 
+import { BrowserRoutes as  Router, Route, Link } from 'react-router-dom';
+
 export default function App() {
   return (
     <div
